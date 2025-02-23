258bfda72f24d13c2a402ef26b29ab3539f4c5b1
od1l1909 <odilbekabdugafurov7@gmail.com>
2025-02-23 14:07:43 +0300

    feat: добавил страницу HomePage

--- src/features/cardPartners/CardPartners.jsx
+++ src/features/cardPartners/CardPartners.jsx
@@ -1,6 +1,16 @@
-import "./cardPartners.scss"
-export const CardPartners = () => {
+import React from 'react';
+import "./CardPartners.scss";
+import librarypartnersicon from "../../shared/libraryimg/librarypartnersicon.png";
+
+export function CardPartners() {
   return (
-    <div>CardPartners</div>
-  )
+    <div className="card-partners">
+      <h2>ПАРТНЕРЫ</h2>
+      <div className="row">
+        <div className="card-partners__card">
+          <img src={librarypartnersicon} alt="Партнеры" className="card-partners__image" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,18 @@
-import { CardNews, CardAfisha, CardBook } from "../../features/index"
-import "./homePage.scss"
+import { HomeBanner, HomeCatalogs,  HomeNews,  HomeRating, HomeViews } from "../../widgets";
+import { CardAfisha,CardPartners} from "../../features/index"
+
 export const HomePage = () => {
   return (
-    <div className=" container">
-      
-    home
+    <div>
+      <h1>Home</h1>
+      <HomeBanner />    {/*адаптив не готов */}
+      <CardAfisha />  {/* готов*/}
+      <HomeCatalogs />  {/*готов */}
+      <HomeNews />  {/*  не готов */}
+      <HomeViews /> {/*готов */}
+      <CardPartners /> {/* готов */}
+      <HomeRating />{/*  не готов*/}
+
     </div>
-  )
+  );
 }
