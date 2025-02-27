b41ba03db316543d9b15f3f93b461bfe31c17af5
Bekislam09 <email@example.com>
2025-02-27 15:42:38 +0600

    fead about page

--- src/widgets/AboutPageComponents/activities/Activities.jsx
+++ src/widgets/AboutPageComponents/activities/Activities.jsx
@@ -4,37 +4,40 @@ import vector2 from '../../../shared/libraryImages/Vector (1).png'
 
 
 export const Activities = () => {
+    const cards = [
+        {
+            id: 1,
+            title: 'Управление качеством услуг',
+            description: 'Aкты и регламенты помогают стандартизировать процессы внутри библиотеки, чтобы обеспечить высокий уровень обслуживания пользователей и соответствие всем требованиям.'
+        },
+        {
+            id: 2,
+            title: 'Соблюдение законодательства',
+            description: 'Эти документы позволяют библиотеке действовать в рамках закона, соблюдая как национальные, так и международные нормы в области интеллектуальной собственности.'
+        },
+        {
+            id: 3,
+            title: 'Эффективность работы',
+            description: 'Регулирующие акты помогают оптимизировать процессы работы с читателями, улучшая доступ к материалам, условия для образовательных и культурных инициатив.'
+        },
+        {
+            id: 4,
+            title: 'Прозрачность и ответственность',
+            description: 'Соблюдение норм и процедур способствует открытости работы библиотеки, повышает доверие пользователей и дает возможность отслеживать эффективность работы каждого отдела.'
+        }
+    ]
     return (
         <div className='activities-container' >
             <h1 className='activities-container-title' >АКТЫ, РЕГУЛИРУЮЩИЕ ДЕЯТЕЛЬНОСТЬ</h1>
             <div className='activities-container-cards' >
-                <div className='activities-container-cards-card' >
-                    <img className='activities-container-cards-card-img' src={vector1} alt="" />
-                    <h3 className='activities-container-cards-card-title'>Управление качеством услуг</h3>
-                    <p className='activities-container-cards-card-description'>Акты и регламенты помогают стандартизировать процессы внутри библиотеки, 
-                        чтобы обеспечить высокий уровень обслуживания пользователей и соответствие 
-                        всем требованиям.</p>
-                </div>
-                <div className='activities-container-cards-card'>
-                    <img className='activities-container-cards-card-img' src={vector2} alt="" />
-                    <h3 className='activities-container-cards-card-title'>Соблюдение законодательства</h3>
-                    <p className='activities-container-cards-card-description'>Эти документы позволяют библиотеке действовать в рамках закона, 
-                        соблюдая как национальные, так и международные нормы в области 
-                        интеллектуальной собственности.</p>
-                </div>
-                <div className='activities-container-cards-card'>
-                    <img className='activities-container-cards-card-img' src={vector1} alt="" />
-                    <h3 className='activities-container-cards-card-title'>Эффективность работы</h3>
-                    <p className='activities-container-cards-card-description third-descritption'>Регулирующие акты помогают оптимизировать процессы работы с читателями, 
-                    улучшая доступ к материалам, условия для образовательных и культурных инициатив.</p>
-                </div>
-                <div className='activities-container-cards-card'>
-                    <img className='activities-container-cards-card-img' src={vector2} alt="" />
-                    <h3 className='activities-container-cards-card-title'>Прозрачность и ответственность</h3>
-                    <p className='activities-container-cards-card-description'>Соблюдение норм и процедур способствует открытости работы библиотеки, 
-                        повышает доверие пользователей и дает возможность отслеживать 
-                        эффективность работы каждого отдела.</p>
-                </div>
+            {
+                    cards.map((cards) => (
+                        <div className='activities-container-cards-card' >
+                            <h3 className='activities-container-cards-card-title'>{cards.title}</h3>
+                            <p className='activities-container-cards-card-description'>{cards.description}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -6,8 +6,8 @@ export {HomeCatalogs} from './HomePageComponents/HomeCatalogs/HomeCatalogs';
 export {HomeNews} from './HomePageComponents/HomeNews/HomeNews';
 export {HomeRating} from './HomePageComponents/HomeRating/HomeRating';
 export {HomeViews} from './HomePageComponents/HomeViews/HomeViews';
-export { Caption } from './AboutPageComponents/caption/Caption'
-export { Management } from './AboutPageComponents/management/Management'
-export { Structure } from './AboutPageComponents/structure/Structure'
-export { Activities } from './AboutPageComponents/activities/Activities'
-export { History } from './AboutPageComponents/history/History'
+export { AboutBanner } from './AboutPageComponents/aboutBanner/AboutBanner';
+export { Management } from './AboutPageComponents/management/Management';
+export { Structure } from './AboutPageComponents/structure/Structure';
+export { Activities } from './AboutPageComponents/activities/Activities';
+export { History } from './AboutPageComponents/history/History';
