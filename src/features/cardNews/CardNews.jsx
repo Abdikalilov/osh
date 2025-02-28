3c305992babbe57c233bf8ec2c724e7716653091
od1l1909 <odilbekabdugafurov7@gmail.com>
2025-02-28 16:30:55 +0300

    add button

--- src/features/cardNews/CardNews.jsx
+++ src/features/cardNews/CardNews.jsx
@@ -16,20 +16,6 @@ const newsItems = [
     description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
     image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
   },
-   {
-    date: "20 фев",
-    time: "19:00",
-    title: "Концерт \"Музыкальные ритмы города\"",
-    description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
-    image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
-  },
-  {
-    date: "20 января 2025",
-    time: "19:00",
-    title: "Концерт \"Музыкальные ритмы города\"",
-    description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
-    image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
-  }, 
   {
     date: "20 фев",
     time: "19:00",
@@ -43,7 +29,21 @@ const newsItems = [
     title: "Концерт \"Музыкальные ритмы города\"",
     description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
     image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
-  }, 
+  },
+  {
+    date: "20 фев",
+    time: "19:00",
+    title: "Концерт \"Музыкальные ритмы города\"",
+    description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
+    image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
+  },
+  {
+    date: "20 января 2025",
+    time: "19:00",
+    title: "Концерт \"Музыкальные ритмы города\"",
+    description: "Окунитесь в атмосферу живой музыки с участием популярных исполнителей.",
+    image: "https://falstaff.b-cdn.net/storage/sites/2/2023/09/Bildschirmfoto_2023-09-28_um_16.52.07.png"
+  },
   {
     date: "20 фев",
     time: "19:00",
@@ -64,21 +64,25 @@ export function CardNews() {
   return (
     <div className="card-news">
       <h2>НОВОСТИ</h2>
-      <div className="news-row">
-        {newsItems.map((news, index) => (
-          <div className="news-col-3" key={index}>
-            <div className="news-card">
-              <img src={news.image} alt={news.title} className="news-image" />
-              <div className="news-content">
-                <h3>{news.title}</h3>
-                <hr />
-                <span className="news-date">{news.date} - {news.time}</span>
-                <p>{news.description}</p>
-                <button className="news-button">Подробнее</button>
+      <div>
+        <div className="news-row">
+          {newsItems.map((news, index) => (
+            <div className="news-col-3" key={index}>
+              <div className="news-card">
+                <img src={news.image} alt={news.title} className="news-image" />
+                <div className="news-content">
+                  <h3>{news.title}</h3>
+                  <hr />
+                  <span className="news-date">{news.date} - {news.time}</span>
+                  <p>{news.description}</p>
+                  <button className="news-button">Подробнее</button>
+                </div>
+
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <button className="news-button-mobile">Подробнее</button>
       </div>
     </div>
   );
