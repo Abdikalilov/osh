34e38e77e5dd667677c8530d1a8010499e980b51
askarov044 <dinmuhammed419@gmail.com>
2025-03-04 16:20:49 +0500

    наш проект

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -6,3 +6,6 @@ export {HomeCatalogs} from './HomePageComponents/HomeCatalogs/HomeCatalogs';
 export {HomeNews} from './HomePageComponents/HomeNews/HomeNews';
 export {HomeRating} from './HomePageComponents/HomeRating/HomeRating';
 export {HomeViews} from './HomePageComponents/HomeViews/HomeViews';
+export {ProjectBanner} from './ourProjects/ProjectBanner/ProjectBanner';
+export {MainProjects} from './ourProjects/MainProject/MainProjects';
+export {AmericanCorner} from './ourProjects/AmericanCorner/AmericanCorner';
