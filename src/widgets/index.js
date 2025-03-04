00b4bbc93eb55427d70fb580d164d32a995e1a10
wwraax <rayhonarahimova08@gmail.com>
2025-03-04 18:01:42 +0600

    create support and electronic page

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,8 +1,15 @@
 export { Header } from './header/Header';
 export { Footer } from './footer/Footer';
-export {Breadcrumb} from "./breadcrumb/Breadcrumb"
-export {HomeBanner} from './HomePageComponents/HomeBanner/HomeBanner';
-export {HomeCatalogs} from './HomePageComponents/HomeCatalogs/HomeCatalogs';
-export {HomeNews} from './HomePageComponents/HomeNews/HomeNews';
-export {HomeRating} from './HomePageComponents/HomeRating/HomeRating';
-export {HomeViews} from './HomePageComponents/HomeViews/HomeViews';
+export { Breadcrumb } from "./breadcrumb/Breadcrumb"
+export { HomeBanner } from './HomePageComponents/HomeBanner/HomeBanner';
+export { HomeCatalogs } from './HomePageComponents/HomeCatalogs/HomeCatalogs';
+export { HomeNews } from './HomePageComponents/HomeNews/HomeNews';
+export { HomeRating } from './HomePageComponents/HomeRating/HomeRating';
+export { HomeViews } from './HomePageComponents/HomeViews/HomeViews';
+export { SupportBanner } from './SupportPageComponents/SupportBanner/SupportBanner';
+export { SupportPartners } from './SupportPageComponents/SupportPartners/SupportPartners';
+export { SupportPayment } from './SupportPageComponents/SupportPayment/SupportPayment';
+export { SupportValue } from './SupportPageComponents/SupportValue/SupportValue';
+export { ElectronicBanner } from './ElectronicKatalogComponents/electronicBanner/ElectronicBanner';
+export { ElectronicPartners } from './ElectronicKatalogComponents/electronicPartners/ElectronicPartners';
+export { ElectronicSearch } from './ElectronicKatalogComponents/electronicSearch/ElectronicSearch';
