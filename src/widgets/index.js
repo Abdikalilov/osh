69057470409b9be63488fe50c185c1b1996e525b
esrdftgybhuinjkmo <erboltayirbekov001@gmail.com>
2025-03-11 15:53:15 +0600

    fix in catalog page

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -6,3 +6,5 @@ export {HomeCatalogs} from './HomePageComponents/HomeCatalogs/HomeCatalogs';
 export {HomeNews} from './HomePageComponents/HomeNews/HomeNews';
 export {HomeRating} from './HomePageComponents/HomeRating/HomeRating';
 export {HomeViews} from './HomePageComponents/HomeViews/HomeViews';
+export {CatalogBanner} from './catalogSection/catalogBanner/CatalogBanner';
+export {CatalogInput} from './catalogSection/catalogInput/CatalogInput';
