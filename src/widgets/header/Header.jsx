f2f2242dbad668a173385ab6795a9ee266c9c1ee
ipolotov1029 <ipolotov30@gmail.com>
2025-03-14 15:05:55 +0600

    fix-in header

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -1,116 +1,115 @@
-import './header.scss';
-import { useState, useRef, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import Group from '../../shared/image/Group.svg';
-import RUS from '../../shared/image/image 2 (1).svg';
-import Icon from '../../shared/image/Icon.svg';
-import { CiLocationOn } from "react-icons/ci";
-import { FiPhone } from "react-icons/fi";
-import { FaEyeSlash } from "react-icons/fa";
-import { MdOutlineLocationOn } from "react-icons/md";
-import { MdOutlinePhone } from "react-icons/md";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import Kg from '../../shared/image/image 3.svg';
-import { IoIosArrowDown } from "react-icons/io";
+import "./header.scss";
+import "./BurgerMenu.jsx"
+import { NavLink } from "react-router-dom";
+import Group from "../../shared/image/Group.svg";
+import Icon from "../../shared/image/Icon.svg";
+import Rus from "../../shared/image/image 2 (1).svg";
+import Kg from "../../shared/image/image 3.svg";
+import { FaEyeSlash, FaInstagram, FaFacebook } from "react-icons/fa";
+import { MdOutlineLocationOn, MdOutlinePhone } from "react-icons/md";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "../../i18n";
+import BurgerMenu from "./BurgerMenu.jsx";
 
-export const Header = () => {
-  const [selected, setSelected] = useState(Kg);
+
+
+const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
-  const selectRef = useRef(null);
 
-  const options = [
-    { value: "1", label: Kg },
-    { value: "2", label: RUS },
-  ];
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (selectRef.current && !selectRef.current.contains(event.target)) {
-        setIsOpen(false);
-      }
-    };
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setIsOpen(false); 
+  };
 
   return (
-    <div className='header container'>
-      <div className='header_top'>
-        <img src={Group} alt="" className='header_top_logo' />
-        <div className='header_top_inp'>
-          <input type="text" placeholder='Поиск' />
-          <button><img src={Icon} alt="" /></button>
-        </div >
-        <button className='header_top_eye'><FaEyeSlash color='#105B60' className='eye' /></button>
+    <div className="language-switcher">
+      <div className="selected-lang" onClick={() => setIsOpen(!isOpen)}>
+        <img
+          src={i18n.language === "ru" ? Rus : Kg}
+          alt={i18n.language === "ru" ? "Russia" : "Kyrgyzstan"}
+          style={{ width: 30, height: 20 }}
+        />
+        <MdOutlineKeyboardArrowDown size={20} style={{
+          color: "white",
+        }}/>
+      </div>
+      {isOpen && (
+        <div className="dropdown">
+          <div onClick={() => changeLanguage("ru")} style={{color: "white"}}>
+            <img src={Rus} alt="Russia" style={{ width: 30, height: 20 }} />ru
+          </div>
+          <div onClick={() => changeLanguage("kg")} style={{color: "white"}}>
+            <img  src={Kg} alt="Kyrgyzstan" style={{ width: 30, height: 20 }} />kg
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LanguageSwitcher;
+
+
+export const Header = () => {
+  return (
+    <div className="header container">
+      <div className="header_top">
+        <img src={Group} alt="Logo" className="header_top_logo" />
+        <div className="header_top_inp">
+          <input type="text" placeholder="Поиск" />
+          <button>
+            <img src={Icon} alt="Search" />
+          </button>
+        </div>
+        <button className="header_top_eye">
+          <FaEyeSlash color="#105B60" className="eye" />
+        </button>
         <nav>
-          <NavLink className='link'>О библиотеке</NavLink>
-          <NavLink className='link'>Поддержать библиотеку</NavLink>
-          <NavLink className='link'>Новости</NavLink>
-          <NavLink className='link'>Услуги</NavLink>
-          <NavLink className='link'>Каталог</NavLink>
+          <NavLink className="link">О библиотеке</NavLink>
+          <NavLink className="link">Поддержать библиотеку</NavLink>
+          <NavLink className="link">Новости</NavLink>
+          <NavLink className="link">Услуги</NavLink>
+          <NavLink className="link">Каталог</NavLink>
         </nav>
       </div>
+
       <div className="header_bottom">
         <nav>
-          <NavLink className='link'>Афиша мероприятий</NavLink>
-          <NavLink className='link'>Профессиональная деятельность</NavLink>
-          <NavLink className='link'>Читателям</NavLink>
-          <NavLink className='link'>Проекты</NavLink>
+          <NavLink className="link">Афиша мероприятий</NavLink>
+          <NavLink className="link">Профессиональная деятельность</NavLink>
+          <NavLink className="link">Читателям</NavLink>
+          <NavLink className="link">Проекты</NavLink>
         </nav>
+
         <div className="header_bottom_social">
-          <div className='header_bottom_social_map'>
-            <div className='header_bottom_social_map_text'>
-              <div className='laug'>
-                <div ref={selectRef} className="">
-                  <div
-                    onClick={() => setIsOpen(!isOpen)}
-                  >
-                    <img  className='laug_img' src={selected} alt="" />
-                    <IoIosArrowDown />
-
-                  </div>
-                  {isOpen && (
-                    <div className="abs ">
-                      {options.map((option) => (
-                        <div
-
-                        style={{marginTop:'10px'}}
-                          key={option.value}
-                          onClick={() => {
-                            setSelected(option.label);
-                            setIsOpen(false);
-                          }}
-                        >
-                          <img  className='aman' src={option.label} alt="" />
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className='header_bottom_social_map_img'>
-                <div className='locate'>
+          <div className="header_bottom_social_map">
+            <div className="header_bottom_social_map_text">
+              <LanguageSwitcher />
+              <div className="header_bottom_social_map_img">
+                <div className="locate">
                   <MdOutlineLocationOn />
                 </div>
-                <div className='locate'>
+                <div className="locate">
                   <MdOutlinePhone />
                 </div>
-                <div className='locate'>
+                <div className="locate">
                   <FaInstagram />
                 </div>
-                <div className='locate'>
+                <div className="locate">
                   <FaFacebook />
                 </div>
+
+                <BurgerMenu />
+
               </div>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
-
-
