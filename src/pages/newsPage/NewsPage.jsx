aeb45d82f7a57da45921ba1ce2b05c33356e2a86
Dena_ <155665379+DenaUxUi@users.noreply.github.com>
2025-03-15 12:08:27 +0600

    Re:worked page

--- src/pages/newsPage/NewsPage.jsx
+++ src/pages/newsPage/NewsPage.jsx
@@ -1,10 +1,15 @@
+import { NewsAdvertisment } from "../../widgets/NewsPageComponents/NewsAdvertisment/NewsAdvertisment";
 import { NewsSection} from "../../widgets/NewsPageComponents/NewsBanner/NewsSection";
+import { NewsBooksUpdate } from "../../widgets/NewsPageComponents/NewsBooksUpdate/NewsBooksUpdate";
 
 
 export const NewsPage = () => {
   return (
     <div>
       <NewsSection />    {/*адаптив готов */}
+      <NewsAdvertisment title="Обьявление о мероприятиях"/>
+      <NewsBooksUpdate />
+      <NewsAdvertisment title="Сми о нас"/>
     </div>
   );
 }
