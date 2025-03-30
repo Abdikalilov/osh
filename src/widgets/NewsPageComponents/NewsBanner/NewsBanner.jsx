a1bd442088164b8dc2657f2987bdfb84610db6a9
Nuraliev-Islam <islamnuraliev2020@gmail.com>
2025-03-30 22:06:19 +0300

    fdsaa

--- src/widgets/NewsPageComponents/NewsBanner/NewsBanner.jsx
+++ src/widgets/NewsPageComponents/NewsBanner/NewsBanner.jsx
@@ -1,10 +1,7 @@
-import './newsBanner.scss'
+import '../../../styles/newsBanner.scss';
 import nenoigirl from '../../../shared/img/books.svg'
 import nenoibook from '../../../shared/img/group.svg'
 import laptop from '../../../shared/img/laptop.svg'
-
-
-
 export const NewsBanner = () => {
     return (
         <div >
